9075d2a991acd43033b951f07c88e5bf66cdf410
kekedezha <dezhac6@gmail.com>
2024-08-06 22:11:39 -0400

    updated game, need to finish and add game into a loop to ask user if they want to cont

--- guess_number.py
+++ guess_number.py
@@ -2,7 +2,20 @@
 import random
 
 def random_number_game(name):
-    player_choice = input(f"\n{name}, guess what number I am thinking of...1, 2 or 3.")
+    game_count = 0
+    player_wins = 0
+
+    random_number = random.choice("123")
+    player_choice = input(f"\n{name}, guess what number I am thinking of...1, 2 or 3.\n\n")
+
+    print(f"\n{name} you chose {player_choice}.")
+    print(f"I was thinking about the number {random_number}")
+
+    if player_choice == random_number:
+        player_wins += 1
+
+    print(f"Game count: {game_count}")
+    print(f"{name}'s wins: {player_wins}")
 
 if __name__ == "__main__":
     import argparse
