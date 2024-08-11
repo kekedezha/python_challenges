631c0205575624f20e925045134f527ccae64447
kekedezha <dezhac6@gmail.com>
2024-08-10 22:55:25 -0400

    completed guessing number game, now have to add arcade file

--- guess_number.py
+++ guess_number.py
@@ -1,4 +1,5 @@
 #number guessing game
+import sys
 import random
 
 def random_number_game(name):
@@ -13,7 +14,7 @@ def random_number_game(name):
         player_choice = input(f"\n{name}, guess what number I am thinking of...1, 2 or 3.\n\n")
 
         print(f"\n{name} you chose {player_choice}.")
-        print(f"I was thinking about the number {random_number}")
+        print(f"I was thinking about the number {random_number}.\n")
 
         
         if player_choice == random_number:
@@ -22,9 +23,24 @@ def random_number_game(name):
 
         print(f"Game count: {game_count}")
         print(f"{name}'s wins: {player_wins}")
+        print(f"Your winning percentage is {(player_wins/game_count):0.2%}\n")
+
+        while True:
+            play_again = input(f"Play again, {name}?\nY for Yes or\nQ to Quit\n")
+            if play_again.lower() not in ["y","q"]:
+                continue
+            else:
+                break
+
+        if play_again == "y":
+            return play_game()
+        else:
+            print(f"Thank you for playing!")
+            sys.exit(f"Bye {name}!")
 
     return play_game
 
+#only run file if in 
 if __name__ == "__main__":
     import argparse
 
