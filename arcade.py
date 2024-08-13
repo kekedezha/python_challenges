ad30d609f5099451297fe80d532e588fb959187c
kekedezha <dezhac6@gmail.com>
2024-08-12 22:12:37 -0400

    added most of logic for arcade file but need to slightly adjust to run correct functions from imports

--- arcade.py
+++ arcade.py
@@ -1,10 +1,23 @@
 from guess_number import random_number_game
 from rps import rps
+import sys
 
 def arcade(name="Player_one"):
     print(f"\n{name}, welcome to the arcade!🕹️\n")
 
-    player_choice = input(f"Please choose a game to play: \n1 = Rock, Paper, Scissors\n2 = Guess My Number\n\nOr press 'x' to exit the arcade.\n\n")
+    while True:
+        player_choice = input(f"Please choose a game to play: \n1 = Rock, Paper, Scissors\n2 = Guess My Number\n\nOr press 'x' to exit the arcade.\n\n")
+        if player_choice.lower() not in ["1","2","x"]:
+            continue
+        else:
+            break
+
+    if player_choice == "2":
+        random_number_game(name)
+    elif player_choice == "1":
+        rps(name)
+    else:
+        sys.exit(f"\n\nSee you next time!\nBye {name}!✌🏽")
 
 #only run file if in 
 if __name__ == "__main__":
